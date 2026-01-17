3d1e361768197b43a9fc04612f5f4ac6daaffc7a
k1414k <k14146k@naver.com>
2026-01-18 03:46:00 +0900

    items_controllerにActiveStorageもjsonとして返すように修正

--- app/controllers/v1/items_controller.rb
+++ app/controllers/v1/items_controller.rb
@@ -3,12 +3,21 @@ class V1::ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
   
   def index
-    @items = Item.all
-    render json: @items
+    items = Item.includes(images_attachments: :blob) # N+1 対策
+    render json: items.map { |item|
+      {
+        **item.as_json,
+        image: item.images.attached? ? url_for(item.images.first) : nil #thumbnail
+      }
+    }
   end
 
   def show
-    render json: @item
+    item = Item.includes(images_attachments: :blob).find(params[:id])
+    render json: {
+        **item.as_json,
+        images: item.images.map { |img| url_for(img) }
+      }
   end
 
   def create
