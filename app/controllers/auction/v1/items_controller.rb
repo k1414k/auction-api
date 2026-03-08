02b69a6bbfb3d2349d0f283d6d9182ce796e72a7
k1414k <k14146k@naver.com>
2026-03-08 11:38:21 +0900

    rollback>STEP=1

--- app/controllers/auction/v1/items_controller.rb
+++ app/controllers/auction/v1/items_controller.rb
@@ -1,31 +1,27 @@
 class Auction::V1::ItemsController < ApplicationController
-  include Rails.application.routes.url_helpers
-
   before_action :set_item, only: [:show, :update, :destroy]
-  before_action :authenticate_user!, only: [:create]
-
+  before_action :authenticate_user!, only: [:create] # index/show は未ログインでも閲覧可
+  
   def index
-    items = Item.includes(images_attachments: :blob)
-
+    items = Item.includes(images_attachments: :blob) # N+1 対策
     render json: items.map { |item|
       {
         **item.as_json,
         is_favorited: current_user ? current_user.favorited?(item) : false,
-        image: item.images.attached? ? public_blob_url(item.images.first) : nil
+        image: item.images.attached? ? url_for(item.images.first) : nil # thumbnail
       }
     }
   end
 
   def show
     item = Item.includes(images_attachments: :blob).find(params[:id])
-
     render json: {
-      **item.as_json,
-      user_nickname: item.user.nickname,
-      created_by_current_user: current_user&.id == item.user_id,
-      is_favorited: current_user&.favorited?(item) || false,
-      images: item.images.map { |img| public_blob_url(img) }
-    }
+        **item.as_json,
+        user_nickname: item.user.nickname,
+        created_by_current_user: current_user&.id == item.user_id,
+        is_favorited: current_user&.favorited?(item) || false,
+        images: item.images.map { |img| url_for(img) }
+      }
   end
 
   def create
@@ -34,7 +30,6 @@ class Auction::V1::ItemsController < ApplicationController
     p[:trading_status] = p[:trading_status].present? ? p[:trading_status].to_i : Item.trading_statuses[:listed]
 
     @item = current_user.items.build(p)
-
     if @item.save
       render json: @item, status: :created
     else
@@ -62,23 +57,6 @@ class Auction::V1::ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(
-      :title,
-      :description,
-      :price,
-      :category_id,
-      :favorites,
-      :condition,
-      :trading_status,
-      images: []
-    )
-  end
-
-  def public_blob_url(blob_or_attachment)
-    rails_blob_url(blob_or_attachment, host: public_api_host)
-  end
-
-  def public_api_host
-    ENV["PUBLIC_API_BASE_URL"] || "http://localhost:3000"
+    params.require(:item).permit(:title, :description, :price, :category_id, :favorites, :condition, :trading_status, images: [])
   end
 end
